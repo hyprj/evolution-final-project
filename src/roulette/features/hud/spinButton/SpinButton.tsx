459bfceeace6d68243b724140f59a4dde22593d1
hyprj <Krzysztofzur21@gmail.com>
2023-04-19 22:32:20 +0200

    fix: disable spin btn while spinning

--- src/roulette/features/hud/spinButton/SpinButton.tsx
+++ src/roulette/features/hud/spinButton/SpinButton.tsx
@@ -6,11 +6,13 @@ import "./spinButton.css";
 export const SpinButton = observer(() => {
   const { runCameraAnimation } = useUIStore();
   const { phaseStore } = useRootStore();
+
+  const isDisabledClass =
+    phaseStore.phase !== "betting" ? "spin-btn--disabled" : "";
+
   return (
     <button
-      className={`spin-btn ${
-        phaseStore.phase !== "betting" ? "spin-btn--disabled" : ""
-      }`}
+      className={`spin-btn ${isDisabledClass}`}
       onClick={() => {
         phaseStore.spin();
         runCameraAnimation();
